c42849129e433d5059178f7cde72c8202f789faf
Manohar Vanga <manohar.vanga@cern.ch>
2011-11-25 15:52:26 +0100

    spec/tools: add demo config loader utility
    Signed-off-by: Manohar Vanga <manohar.vanga@cern.ch>

---
+++ tools/configload.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+#include <spec.h>
+
+int main(int argc, char *argv[])
+{
+	int fd;
+	struct spec_fw fw;
+	FILE *file;
+	int len;
+	char *buffer;
+	int num;
+	char devname[20];
+
+	if (argc != 3) {
+		printf("usage: %s <spec_num> <firmware>\n", argv[0]);
+		printf("spec_num is used to create /dev/spec<num>\n");
+		exit(1);
+	}
+
+	num = atoi(argv[1]);
+	sprintf(devname, "/dev/spec%d", num);
+
+	/* read in the firmware file */
+	file = fopen(argv[2], "rw");
+
+	fseek(file, 0, SEEK_END);
+	len = ftell(file);
+	fseek(file, 0, SEEK_SET);
+
+	buffer = (char *)malloc(len);
+	if (!buffer) {
+		fprintf(stderr, "Memory error!");
+		fclose(file);
+		return -1;
+	}
+
+	fread(buffer, len, 1, file);
+	fclose(file);
+
+	/* load firmware */
+	fw.fwlen = len;
+	fw.data = buffer;
+	
+	fd = open(devname, O_RDWR);
+	if (fd < 0)
+		return fd;
+	
+	ioctl(fd, SPEC_LOAD_DEMO_CONFIG, &fw);
+
+	free(buffer);
+
+	close(fd);
+	return 0;
+}
